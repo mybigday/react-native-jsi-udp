f96184ef2c97d2c467bfe00c8ea82d67820e1d30
Hans <hans.chen@bricks.tools>
2025-06-16 18:10:43 +0800

    fix: fix build error when use RN >= 0.76

--- example/ios/File.swift
+++
@@ -1,6 +0,0 @@
-//
-//  File.swift
-//  JsiUdpExample
-//
-
-import Foundation
--- example/metro.config.js
+++ example/metro.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-const escape = require('escape-string-regexp');
-const exclusionList = require('metro-config/src/defaults/exclusionList');
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 const pak = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
@@ -9,20 +8,19 @@ const modules = Object.keys({
   ...pak.peerDependencies,
 });
 
-module.exports = {
+/**
+ * Metro configuration
+ * https://reactnative.dev/docs/metro
+ *
+ * @type {import('@react-native/metro-config').MetroConfig}
+ */
+const config = {
   projectRoot: __dirname,
   watchFolders: [root],
 
   // We need to make sure that only one version is loaded for peerDependencies
   // So we block them at the root, and alias them to the versions in example's node_modules
   resolver: {
-    blacklistRE: exclusionList(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`)
-      )
-    ),
-
     extraNodeModules: modules.reduce((acc, name) => {
       acc[name] = path.join(__dirname, 'node_modules', name);
       return acc;
@@ -38,3 +36,5 @@ module.exports = {
     }),
   },
 };
+
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
